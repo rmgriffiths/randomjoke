c6a321be3b437b714a3802973fc6126b6bb10189
Richard Griffiths <rg_in_newcastle@yahoo.co.uk>
2021-02-05 17:05:06 +0000

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,73 +1,73 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
-  server: {
-    port: 8081, // default: 3000
-  },
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    titleTemplate: '%s - nuxtproject',
-    title: 'nuxtproject',
-    htmlAttrs: {
-      lang: 'en',
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // baseURL: 'https://ettcars.herokuapp.com/api',
-    // baseURL: 'https://official-joke-api.appspot.com',
-    baseURL: 'https://v2.jokeapi.dev',
-  },
-
-  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: false,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        titleTemplate: "%s - nuxtproject",
+        title: "nuxtproject",
+        htmlAttrs: {
+            lang: "en",
         },
-      },
+        meta: [
+            { charset: "utf-8" },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1",
+            },
+            { hid: "description", name: "description", content: "" },
+        ],
+        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     },
-  },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
-}
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/eslint
+        "@nuxtjs/eslint-module",
+        // https://go.nuxtjs.dev/vuetify
+        "@nuxtjs/vuetify",
+    ],
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        // https://go.nuxtjs.dev/axios
+        "@nuxtjs/axios",
+    ],
+
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {
+        // baseURL: 'https://ettcars.herokuapp.com/api',
+        // baseURL: 'https://official-joke-api.appspot.com',
+        baseURL: "https://v2.jokeapi.dev",
+    },
+
+    // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+    vuetify: {
+        customVariables: ["~/assets/variables.scss"],
+        theme: {
+            dark: false,
+            themes: {
+                dark: {
+                    primary: colors.blue.darken2,
+                    accent: colors.grey.darken3,
+                    secondary: colors.amber.darken3,
+                    info: colors.teal.lighten1,
+                    warning: colors.amber.base,
+                    error: colors.deepOrange.accent4,
+                    success: colors.green.accent3,
+                },
+            },
+        },
+    },
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {},
+};
