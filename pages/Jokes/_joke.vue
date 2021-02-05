98c80893bdaf8dc125640be2cb9ac53041e10ef0
Richard Griffiths <rg_in_newcastle@yahoo.co.uk>
2021-02-05 15:40:53 +0000

    Update

--- pages/Jokes/_joke.vue
+++ pages/Jokes/_joke.vue
@@ -1,24 +1,28 @@
 <template>
-  <v-container mb-12>
-    {{ joke }}
-  </v-container>
+    <v-container mb-12>
+        Joke of the day
+        <br />
+        {{ joke.setup }}
+        <br />
+        {{ joke.delivery }}
+    </v-container>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      joke: [],
-    }
-  },
-  computed: {},
-  created() {
-    const jokesId = this.$route.params.joke
-    const path = '/jokes/' + jokesId
-    this.$axios.get(path).then((res) => {
-      this.joke = res.data
-    })
-  },
-  methods: {},
-}
+    data() {
+        return {
+            joke: [],
+        };
+    },
+    computed: {},
+    created() {
+        const jokesId = this.$route.params.joke;
+        const path = "/joke/" + jokesId;
+        this.$axios.get(path).then((res) => {
+            this.joke = res.data;
+        });
+    },
+    methods: {},
+};
 </script>
--- pages/random.vue
+++ pages/random.vue
@@ -1,25 +1,26 @@
 <template>
-  <v-container mb-12>
-    Joke of the day
-    {{ joke.setup }}
-    <br />
-    {{ joke.delivery }}
-  </v-container>
+    <v-container mb-12>
+        Joke of the day
+        <br />
+        {{ joke.setup }}
+        <br />
+        {{ joke.delivery }}
+    </v-container>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      joke: [],
-    }
-  },
-  computed: {},
-  created() {
-    const path = '/joke/Any'
-    this.$axios.get(path).then((res) => {
-      this.joke = res.data
-    })
-  },
-}
+    data() {
+        return {
+            joke: [],
+        };
+    },
+    computed: {},
+    created() {
+        const path = "/joke/Any";
+        this.$axios.get(path).then((res) => {
+            this.joke = res.data;
+        });
+    },
+};
 </script>
